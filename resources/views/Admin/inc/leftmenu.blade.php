d4b89845e5a192c19066edd114725d6e031dea9b
Amit0717 <amitcb246@outlook.com>
2021-03-22 14:05:35 +0600

    First commit

---
+++ resources/views/Admin/inc/leftmenu.blade.php
@@ -0,0 +1,77 @@
+<div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li class="sidebar-search">
+                            <div class="input-group custom-search-form">
+                                <input type="text" class="form-control" placeholder="Search...">
+                                <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                            </span>
+                            </div>
+                            <!-- /input-group -->
+                        </li>
+                        <li>
+                            <a href="home"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="glyphicon glyphicon-gift"></i> Items <span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="newItem">Item Entry</a>
+                                </li>
+                                <li>
+                                    <a href="showItem">Show Items</a>
+                                </li>                               
+                                <li>
+                                    <a href="trashedItem">Trashed Items</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="glyphicon glyphicon-envelope"></i> Messages <span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="showMsg">Unread Messages</a>
+                                </li>                               
+                                <li>
+                                    <a href="readMsg">Read Messages</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="glyphicon glyphicon-erase"></i> Info <span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="info">About</a>
+                                </li>                               
+                                <li>
+                                    <a href="info2">Contact</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="glyphicon glyphicon-th-list"></i> Table Reservation <span class="fa arrow"></span> </a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="reservation">Request Table</a>
+                                </li>
+                                <li>
+                                    <a href="booked">Booked Table</a>
+                                </li> 
+                                <li>
+                                    <a href="addTable">Add Table</a>
+                                </li>                               
+                                <li>
+                                    <a href="completed">Delete Table</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
